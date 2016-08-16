0f9c1ebe72220b0818b42c6486b827a0c860cb9d
Matthew Stone <matthew.stone12@gmail.com>
2016-08-16 00:11:15 -0400

    clean up and docs

--- venn.py
+++ venn.py
@@ -6,7 +6,7 @@
 # Distributed under terms of the MIT license.
 
 """
-
+Simple venn diagrams.
 """
 
 import matplotlib.pyplot as plt
@@ -15,110 +15,193 @@ import matplotlib.patches as patches
 
 def venn4(subsets,
           set_labels=('A', 'B', 'C', 'D'),
-          set_colors=['#8f1402', '#0485d1', '#feb308', '#8eab12'],
+          #  set_colors=['#8f1402', '#0485d1', '#feb308', '#8eab12'],
+          set_colors=['#8eab12', '#feb308', '#8f1402', '#0485d1'],
           alpha=0.4,
           ax=None,
           set_label_fontsize=18,
-          subset_label_fontsize=14):
-    
+          subset_label_fontsize=14,
+          rotate_labels=True):
+    """
+    Plot a four-way venn diagram.
+
+    Parameters
+    ----------
+    subsets : list
+        Values for each subset of Venn.
+        May be int, float, or string. Use strings for any custom formatting
+        [A, B, C, D, AB, AC, AD, BC, BD, CD, ABC, ABD, ACD, BCD, ABCD]
+    set_labels : list of str, optional
+        Labels around ellipses' exterior
+    set_colors : list, optional
+        Colors of Venn ellipses.
+        Defaults to xkcd's [pea green, amber, brick red, cerulean]
+    alpha : float, optional
+        Alpha of Venn ellipses
+    ax : AxesSubplot
+        Axis to draw Venn on
+    set_label_fontsize : int, optional
+        Fontsize of exterior set labels.
+        Default=18pt, optimized for 8in by 8in figure
+    subset_label_fontsize : int, optional
+        Fontsize of interior count labels
+        Default=14pt, optimized for 8in by 8in figure
+    rotate_labels : bool, optional
+        When true, rotate count labels to fit larger numbers.
+        When false, count labels are horizontal with rotation=0.
+
+    Returns
+    -------
+    ax : AxesSubplot
+    """
+
+    if len(subsets) != 15:
+        raise Exception('Must provide exactly 15 subset values')
+
     if ax is None:
         ax = plt.gca()
 
     width = 0.75
     height = 0.5
-    ellipse_angle = 45
     alpha = 0.4
-    text_angle = 45
 
-    e = patches.Ellipse((0.65, 0.4), width, height, ellipse_angle, alpha=alpha, facecolor=set_colors[0])
-    ax.add_patch(e)
-    ax.text(0.17, 0.92, set_labels[0], rotation=45, fontsize=set_label_fontsize)
+    # Draw ellipses
+    ellipse_coords = [
+        ((0.50, 0.60), -45),  # A (top left)
+        ((0.50, 0.60),  45),  # B (top right)
+        ((0.65, 0.40),  45),  # C (bottom right)
+        ((0.35, 0.40), -45),  # D (bottom left)
+    ]
+    for (coord, angle), color in zip(ellipse_coords, set_colors):
+        e = patches.Ellipse(coord, width, height, angle,
+                            alpha=alpha, facecolor=color)
+        ax.add_patch(e)
 
-    e = patches.Ellipse((0.35, 0.4), width, height, -ellipse_angle, alpha=alpha, facecolor=set_colors[1])
-    ax.add_patch(e)
-    ax.text(0.73, 0.92, set_labels[1], rotation=-45, fontsize=set_label_fontsize)
+    # Add exterior set labels
+    set_label_positions = [
+        (0.17, 0.92,  45),  # A (top)
+        (0.73, 0.92, -45),  # B (left)
+        (0.05, 0.26, -45),  # C (right)
+        (0.85, 0.28,  50),
+    ]
+    for label, (x, y, rotation) in zip(set_labels, set_label_positions):
+        ax.text(x, y, label, rotation=rotation,
+                ha='center', va='center', fontsize=set_label_fontsize)
 
-    e = patches.Ellipse((0.5, 0.6), width, height, ellipse_angle, alpha=alpha, facecolor=set_colors[2])
-    ax.add_patch(e)
-    ax.text(0.05, 0.26, set_labels[2], rotation=-45, fontsize=set_label_fontsize)
-
-    e = patches.Ellipse((0.5, 0.6), width, height, -ellipse_angle, alpha=alpha, facecolor=set_colors[3])
-    ax.add_patch(e)
-    ax.text(0.85, 0.28, set_labels[3], rotation=50, fontsize=set_label_fontsize)
-
-    # D, L, M, W, DL, DM, DW, LM, LW, MW, DLM, DLW, DMW, LMW, DLMW
-    
+    # Add subset count labels
     subsets = [str(s) for s in subsets]
-    # ax.text(0.25, 0.8, subsets[0], va='center', rotation=45, fontsize=14)
-    ax.text(0.3, 0.83, subsets[0], va='center', ha='center', rotation=45, fontsize=subset_label_fontsize)
-    ax.text(0.7, 0.83, subsets[1], va='center', ha='center', rotation=-45, fontsize=subset_label_fontsize)
-    ax.text(0.18, 0.3, subsets[2], va='center', ha='center', rotation=-45, fontsize=subset_label_fontsize)
-    ax.text(0.83, 0.3, subsets[3], va='center', ha='center', rotation=45, fontsize=subset_label_fontsize)
-    ax.text(0.5, 0.77, subsets[4], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize) # DL
-    ax.text(0.22, 0.68, subsets[5], va='center', ha='center', rotation=55, fontsize=subset_label_fontsize) # DM
-    ax.text(0.75, 0.4, subsets[6], va='center', ha='center', rotation=45, fontsize=subset_label_fontsize) # DW
-    ax.text(0.25, 0.4, subsets[7], va='center', ha='center', rotation=-45, fontsize=subset_label_fontsize) # LM
-    ax.text(0.77, 0.68, subsets[8], va='center', ha='center', rotation=-55, fontsize=subset_label_fontsize) # LW
-    ax.text(0.5, 0.18, subsets[9], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize) # MW
-    ax.text(0.33, 0.58, subsets[10], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize) # DLM
-    ax.text(0.66, 0.58, subsets[11], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize) # DLW
-    ax.text(0.6, 0.32, subsets[12], va='center', ha='center', rotation=10, fontsize=subset_label_fontsize) # DMW
-    ax.text(0.4, 0.32, subsets[13], va='center', ha='center', rotation=-10, fontsize=subset_label_fontsize) # LMW
-    ax.text(0.5, 0.45, subsets[14], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize) # DLMW
+    subset_positions = [
+        (0.30, 0.83,  45),  # A
+        (0.70, 0.83, -45),  # B
+        (0.18, 0.30, -45),  # C
+        (0.83, 0.30, -45),  # D
+        (0.50, 0.77,   0),  # AB
+        (0.22, 0.68,  55),  # AC
+        (0.75, 0.40,  45),  # AD
+        (0.25, 0.40, -45),  # BC
+        (0.77, 0.68, -55),  # BD
+        (0.50, 0.18,   0),  # CD
+        (0.33, 0.58,   0),  # ABC
+        (0.66, 0.58,   0),  # ABD
+        (0.60, 0.32,  10),  # ACD
+        (0.40, 0.32, -10),  # BCD
+        (0.50, 0.45,   0),  # ABCD
+    ]
+    for label, (x, y, rotation) in zip(subsets, subset_positions):
+        ax.text(x, y, label, rotation=rotation,
+                ha='center', va='center', fontsize=subset_label_fontsize)
 
     # Remove borders
     ax.set_xticklabels('')
     ax.set_yticklabels('')
     ax.axis('off')
-    
-    # Trim whitespace
-#     ax.set_ylim(0.07, 0.96)
+
     ax.set_aspect('equal')
 
     return ax
 
+
 def venn3(subsets,
           set_labels=('A', 'B', 'C'),
-          set_colors=['#8f1402', '#0485d1', '#feb308'],
+          set_colors=['#feb308', '#8f1402', '#0485d1'],
           alpha=0.4,
           ax=None,
           set_label_fontsize=18,
           subset_label_fontsize=14):
+    """
+    Plot a three-way venn diagram.
+
+    Parameters
+    ----------
+    subsets : list
+        Values for each subset of Venn.
+        May be int, float, or string. Use strings for any custom formatting
+        [A, B, C, AB, AC, BC, ABC]
+    set_labels : list of str, optional
+        Labels around ellipses' exterior
+    set_colors : list, optional
+        Colors of Venn ellipses.
+        Defaults to xkcd's [amber, brick red, cerulean]
+    alpha : float, optional
+        Alpha of Venn ellipses
+    ax : AxesSubplot
+        Axis to draw Venn on
+    set_label_fontsize : int, optional
+        Fontsize of exterior set labels.
+        Default=18pt, optimized for 8in by 8in figure
+    subset_label_fontsize : int, optional
+        Fontsize of interior count labels
+        Default=14pt, optimized for 8in by 8in figure
+
+    Returns
+    -------
+    ax : AxesSubplot
+    """
+
+    if len(subsets) != 7:
+        raise Exception('Must provide exactly 7 subset values')
 
     if ax is None:
         ax = plt.gca()
 
     width = 0.6
     height = 0.6
-    ellipse_angle = 45
     alpha = 0.4
-    text_angle = 45
-    set_label_fontsize = 18
-    subset_label_fontsize = 14
 
-    e = patches.Ellipse((0.63, 0.37), width, height, alpha=alpha, facecolor=set_colors[0])
-    ax.add_patch(e)
-    ax.text(0.5, 0.97, set_labels[0], rotation=0, ha='center', va='center', fontsize=set_label_fontsize)
+    # Draw ellipses
+    ellipse_coords = [
+        (0.50, 0.63),  # A (top)
+        (0.63, 0.37),  # B (bottom right)
+        (0.37, 0.37),  # C (bottom left)
+    ]
+    for color, coord in zip(set_colors, ellipse_coords):
+        e = patches.Ellipse(coord, width, height, alpha=alpha, facecolor=color)
+        ax.add_patch(e)
 
-    e = patches.Ellipse((0.37, 0.37), width, height, alpha=alpha, facecolor=set_colors[1])
-    ax.add_patch(e)
-    ax.text(0.88, 0.14, set_labels[1], rotation=45, ha='center', va='center', fontsize=set_label_fontsize)
+    # Add exterior set labels
+    set_label_positions = [
+        (0.50, 0.97,   0),  # A (top)
+        (0.88, 0.14,  45),  # B (bottom right)
+        (0.12, 0.14, -45),  # C (bottom left)
+    ]
+    for label, (x, y, rotation) in zip(set_labels, set_label_positions):
+        ax.text(x, y, label, rotation=rotation,
+                ha='center', va='center', fontsize=set_label_fontsize)
 
-    e = patches.Ellipse((0.5, 0.63), width, height, alpha=alpha, facecolor=set_colors[2])
-    ax.add_patch(e)
-    ax.text(0.12, 0.14, set_labels[2], rotation=-45, ha='center', va='center', fontsize=set_label_fontsize)
-
-    # D, L, M, W, DL, DM, DW, LM, LW, MW, DLM, DLW, DMW, LMW, DLMW
-
+    # Add subset count labels
     subsets = [str(s) for s in subsets]
-    # ax.text(0.25, 0.8, subsets[0], va='center', rotation=45, fontsize=14)
-    ax.text(0.5, 0.77, subsets[0], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize)
-    ax.text(0.77, 0.3, subsets[1], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize)
-    ax.text(0.23, 0.3, subsets[2], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize)
-    ax.text(0.7, 0.55, subsets[3], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize)
-    ax.text(0.3, 0.55, subsets[4], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize) # DL
-    ax.text(0.5, 0.24, subsets[5], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize) # DM
-    ax.text(0.5, 0.47, subsets[6], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize) # DW
+    subset_positions = [
+        (0.5, 0.77),  # A
+        (0.77, 0.3),  # B
+        (0.23, 0.3),  # C
+        (0.7, 0.55),  # AB
+        (0.3, 0.55),  # AC
+        (0.5, 0.24),  # BC
+        (0.5, 0.47)   # ABC
+    ]
+    for label, (x, y) in zip(subsets, subset_positions):
+        ax.text(x, y, label, rotation=0,
+                ha='center', va='center', fontsize=subset_label_fontsize)
 
     # Remove borders
     ax.set_xticklabels('')
@@ -128,41 +211,83 @@ def venn3(subsets,
 
     return ax
 
+
 def venn2(subsets,
           set_labels=('A', 'B'),
-          set_colors=['#8f1402', '#0485d1'],
+          set_colors=['#0485d1', '#8f1402'],
           alpha=0.4,
           ax=None,
           set_label_fontsize=18,
           subset_label_fontsize=14):
-    
+    """
+    Plot a two-way venn diagram.
+
+    Parameters
+    ----------
+    subsets : list
+        Values for each subset of Venn.
+        May be int, float, or string. Use strings for any custom formatting
+        [A, B, AB]
+    set_labels : list of str, optional
+        Labels around ellipses' exterior
+    set_colors : list, optional
+        Colors of Venn ellipses.
+        Defaults to xkcd's [cerulean, brick red]
+    alpha : float, optional
+        Alpha of Venn ellipses
+    ax : AxesSubplot
+        Axis to draw Venn on
+    set_label_fontsize : int, optional
+        Fontsize of exterior set labels.
+        Default=18pt, optimized for 8in by 8in figure
+    subset_label_fontsize : int, optional
+        Fontsize of interior count labels
+        Default=14pt, optimized for 8in by 8in figure
+
+    Returns
+    -------
+    ax : AxesSubplot
+    """
+
+    if len(subsets) != 3:
+        raise Exception('Must provide exactly 3 subset values')
+
     if ax is None:
         ax = plt.gca()
-        
+
+    # Circle shape and coloring
     width = 0.65
     height = 0.65
-    ellipse_angle = 45
     alpha = 0.4
-    text_angle = 45
-    set_label_fontsize = 18
-    subset_label_fontsize = 14
 
-    e = patches.Ellipse((0.63, 0.5), width, height, alpha=alpha, facecolor=set_colors[0])
-    ax.add_patch(e)
-    ax.text(0.18, 0.82, set_labels[0], rotation=30, ha='center', va='center', fontsize=set_label_fontsize)
+    # Draw ellipses
+    ellipse_coords = [
+        (0.37, 0.5),  # A (left)
+        (0.63, 0.5),  # B (right)
+    ]
+    for color, coord in zip(set_colors, ellipse_coords):
+        e = patches.Ellipse(coord, width, height, alpha=alpha, facecolor=color)
+        ax.add_patch(e)
 
-    e = patches.Ellipse((0.37, 0.5), width, height, alpha=alpha, facecolor=set_colors[1])
-    ax.add_patch(e)
-    ax.text(0.82, 0.82, set_labels[1], rotation=-30, ha='center', va='center', fontsize=set_label_fontsize)
+    # Add exterior set labels
+    set_label_positions = [
+        (0.18, 0.82, 30),   # A (left)
+        (0.82, 0.82, -30),  # B (right)
+    ]
+    for label, (x, y, rotation) in zip(set_labels, set_label_positions):
+        ax.text(x, y, label, rotation=rotation,
+                ha='center', va='center', fontsize=set_label_fontsize)
 
-    # D, L, M, W, DL, DM, DW, LM, LW, MW, DLM, DLW, DMW, LMW, DLMW
-
+    # Add subset count labels
     subsets = [str(s) for s in subsets]
-    # ax.text(0.25, 0.8, subsets[0], va='center', rotation=45, fontsize=14)
-    ax.text(0.2, 0.5, subsets[0], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize)
-    ax.text(0.8, 0.5, subsets[1], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize)
-    ax.text(0.5, 0.5, subsets[2], va='center', ha='center', rotation=0, fontsize=subset_label_fontsize)
-
+    subset_positions = [
+        (0.2, 0.5),  # A
+        (0.8, 0.5),  # B
+        (0.5, 0.5),  # AB
+    ]
+    for label, (x, y) in zip(subsets, subset_positions):
+        ax.text(x, y, label, rotation=0,
+                ha='center', va='center', fontsize=subset_label_fontsize)
 
     # Remove borders
     ax.set_xticklabels('')
